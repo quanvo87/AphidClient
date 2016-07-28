7360f97258dfd4d0f6d5fc0c931be53d6b910af2
Aaron Liberatore <aml68@duke.edu>
2016-07-28 11:27:32 -0500

    Update Package.swift
    Stays at snapshot 6/20

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "AphidClient",
     dependencies:[
-      .Package(url:"https://github.com/IBM-Swift/Aphid", majorVersion:0, minor: 2)
+      .Package(url:"https://github.com/IBM-Swift/Aphid", majorVersion:0, minor: 1)
     ]
 )
